b0e89aafc4da00d333c85aa9fc55e2c271bab93f
User <olegbashchuk@gmail.com>
2020-06-06 12:29:22 +0300

    person class1

---
+++ src/Scanner.java
@@ -0,0 +1,31 @@
+import java.io.InputStream;
+//import java.util.Scanner;
+public class Scanner {
+    public Scanner(InputStream in) {
+    }
+    public static void main(String[] args) {
+        java.util.Scanner month = new java.util.Scanner(System.in);
+        System.out.println("enter the number of month: ");
+        int w = month.nextint();
+
+        int [] array1 = {w};
+        int k = array1.length;
+        System.out.println(k);
+
+
+
+    }
+}
+
+/*
+
+public class Scaner{
+   public static void main(String[] arg) {
+       for (int one = 1; one <=10; one++){
+           System.out.println("ВИЙШЛО!!!" +1);
+       }
+
+
+   }
+}
+*/
---
+++ src/lesson5/Generic.java
@@ -0,0 +1,20 @@
+/*Write parameterized methods union(Set set1, Set set2) and intersect(Set set1, Set set2),
+realizing the operations of union and intersection of two sets.
+Test the operation of these techniques on two pre-filled sets.
+
+ Create map personMap and add to it ten persons of type <String, String> (lastName, firstName).
+ Output the entities of the map on the screen.
+ There are at less two persons with the same firstName among these 10 people?
+ Remove from the map person whose firstName is ”Orest” (or other). Print result.
+*/
+package lesson5;
+
+public class Generic {
+
+
+    public static void main(String[] args) {
+        System.out.println("Я вивчаю");
+        System.out.println("Java");
+    }
+
+}
