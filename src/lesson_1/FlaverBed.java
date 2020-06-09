5104dcad88d2003935030ef1256a1271ced8de14
User <olegbashchuk@gmail.com>
2020-06-09 12:55:11 +0300

    person class1

--- src/lesson_1/FlaverBed.java
+++ src/lesson_1/FlaverBed.java
@@ -1,22 +1,21 @@
 package lesson_1;
 import java.util.Scanner;
 
-//throws IOException {
-  //      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 public class FlaverBed {
-    public static void main(String[] args, double radius) {
-/*
+    public static void main(String[] args) {
+
+        System.out.println("What is radius?");
+        Scanner rad = new Scanner(System.in);
+        double radius = rad.nextDouble();
+
         double Pi = 3.14;
         double P = 2 * Pi * radius;
         double S = Pi * radius;
        // double radius = Double.parseDouble(br.readLine();
-        System.out.println("What is radius?");
-        double radius = new Scanner(System.in);
-        radius = radius.nextDouble();
+
         System.out.println("Периметр складає " + P);
         System.out.println("Площа складає " + S);
-   */
+
 
     }
 }
