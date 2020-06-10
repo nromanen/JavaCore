0361542978d706e8494e35c8be66f542275cabda
User <olegbashchuk@gmail.com>
2020-06-10 13:06:32 +0300

    person class1

---
+++ src/lesson_3/Person.java
@@ -0,0 +1,36 @@
+package lesson_3;
+
+import java.util.Scanner;
+
+import static jdk.nashorn.tools.ShellFunctions.input;
+
+/*Class Person should consist of
+        three private fields: firstName, lastName and birthYear (the birthday year)
+        properties for access to these fields
+default constructor and constructor with 2 parameters (first and last names)
+        methods:
+        getAge() - to calculate the age of person
+        input() - to input information about person
+        output() - to output information about person
+        changeName(String fn, String fn) - to change the first name or/and last name of person
+        In the method main() create 5 objects of Person type and input information about them.
+
+*/
+public class Person{
+    //
+    private static void main(String[] arg){
+        Scanner imy = new Scanner(System.in);
+
+     String Person1 = input ();
+     String Person2 = input ();
+     String Person3 = input ();
+     String Person4 = input ();
+     String Person5 = input ();
+
+
+
+
+    }
+}
+
+
