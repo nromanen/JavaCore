33cb09381cbe7cbac25af66d7c24e3b36488ad2b
User <olegbashchuk@gmail.com>
2020-06-11 23:05:37 +0300

    person class1

--- src/lesson_3/Person.java
+++ src/lesson_3/Person.java
@@ -1,7 +1,5 @@
 package lesson_3;
-
 import java.util.Scanner;
-
 import static jdk.nashorn.tools.ShellFunctions.input;
 
 /*Class Person should consist of
@@ -14,22 +12,34 @@ default constructor and constructor with 2 parameters (first and last names)
         output() - to output information about person
         changeName(String fn, String fn) - to change the first name or/and last name of person
         In the method main() create 5 objects of Person type and input information about them.
+ String firstName = ();
+     String secondName = ();
+     int birthDay = ();
 
-*/
-public class Person{
-    //
-    private static void main(String[] arg){
-        Scanner imy = new Scanner(System.in);
-
-     String Person1 = input ();
+       String Person1 = input ();
      String Person2 = input ();
      String Person3 = input ();
      String Person4 = input ();
      String Person5 = input ();
+*/
+public class Person{
+    //
+    public static void main(String[] arg){
 
+        System.out.println("Ваше імя?");
+        Scanner imy = new Scanner(System.in);
+        String firstName = imy.nextLine();
+            System.out.println("Ваше прізвище?");
+            Scanner dry = new Scanner(System.in);
+            String secondName = dry.nextLine();
+        System.out.println("Ваш рік народження?");
+        Scanner dat = new Scanner(System.in);
+        int birthDay = dat.nextInt();
 
-
-
+public static void second(String[] arg){
+            String Person1 = (firstName, lastName, birthYear);
+            System.out.println(Person1);
+        }
     }
 }
 
---
+++ src/lesson_4/Operators.java
@@ -0,0 +1,19 @@
+package lesson_4;
+
+public class Operators {
+    public static void main(String[] arg){
+
+int a = 0;
+int b = 5;
+int c = -2;
+
+int max = 5;
+int min = -5;
+
+int diapazon = b|a|c;
+if (a >= min){
+    System.out.println("Дані числа лежать в діапазоні від -5 до 5");
+
+        }
+    }
+}
